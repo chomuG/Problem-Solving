26f3fb28f9d866dc224a2732bd4a8266dfac4e42
chomuG <rudcks1237@naver.com>
2025-04-02 12:31:58 +0900

    [Silver I] Title: 꿈틀꿈틀 호석 애벌레 - 기능성, Time: 0 ms, Memory: 2020 KB, Score: 31 point -BaekjoonHub

---
+++ 백준/Silver/20167. 꿈틀꿈틀 호석 애벌레 － 기능성/꿈틀꿈틀 호석 애벌레 － 기능성.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <map>
+#include <queue>
+#include <deque>
+#include <cstdlib>
+#include <algorithm>
+using namespace std;
+int n, k, maxEnergy;
+vector<int> branch, cache;
+
+
+void findMaxEnergy(int idx, int satisfaction, int energy) {
+	if (idx == n) {
+		maxEnergy = max(maxEnergy, energy);
+		return ;
+	}
+
+	findMaxEnergy(idx + 1, satisfaction, energy);
+
+	satisfaction += branch[idx];
+
+	if (k <= satisfaction) {
+		energy += (satisfaction - k);
+		satisfaction = 0;
+	}
+
+	findMaxEnergy(idx + 1, satisfaction, energy);
+}
+
+
+int main() {
+	cin >> n >> k;
+	branch.assign(n, 0);
+	cache.assign(n, -1);
+
+	for (int i = 0; i < n; ++i) {
+		cin >> branch[i];
+	}
+
+	findMaxEnergy(0, 0, 0);
+
+	cout << maxEnergy << "\n";
+
+	return 0;
+}
